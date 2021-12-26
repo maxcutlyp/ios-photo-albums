3d14aeb7ffa3691ce41e4c75caf50b380aeb2a39
maxcutlyp <max@maxcutlyp.com>
2021-12-26 15:37:22 +0800

    create basic argparse boilerplate

---
+++ iosphotoalbums.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3.10
+
+import argparse
+import sys
+
+def main(dcim_dir: str, photos_sqlite_file: str, output_dir: str) -> int:
+    return 0
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Convert iOS albums into folders')
+    parser.add_argument('dcim_dir', metavar='DCIM', type=str, help='path/to/DCIM')
+    parser.add_argument('photos_sqlite_file', metavar='Photos.sqlite', type=str, help='path/to/Photos.sqlite')
+    parser.add_argument('-o', dest='output_dir', metavar='output', type=str, help='path/to/desired/output', default='.')
+
+    args = parser.parse_args()
+    sys.exit(main(args.dcim_dir, args.photos_sqlite_file, args.output_dir))
