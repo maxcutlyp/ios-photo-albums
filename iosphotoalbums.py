a9167f3c36d0de74af83904f06a827fdb695d1cf
maxcutlyp <max@maxcutlyp.com>
2021-12-26 15:42:31 +0800

    add albumsmetadata arg

--- iosphotoalbums.py
+++ iosphotoalbums.py
@@ -10,6 +10,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Convert iOS albums into folders')
     parser.add_argument('dcim_dir', metavar='DCIM', type=str, help='path/to/DCIM')
     parser.add_argument('photos_sqlite_file', metavar='Photos.sqlite', type=str, help='path/to/Photos.sqlite')
+    parser.add_argument('albumsmetadata_dir', metavar='AlbumsMetadata', type=str, help='path/to/AlbumsMetadata')
     parser.add_argument('-o', dest='output_dir', metavar='output', type=str, help='path/to/desired/output', default='.')
 
     args = parser.parse_args()
